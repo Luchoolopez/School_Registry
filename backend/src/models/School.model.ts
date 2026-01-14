59e7abf7cb07351acba4a189e3d126b7bcad919d
LuchooLopez <luchoolopez2810@gmail.com>
2026-01-14 02:20:06 -0300

    Creado modelo de school

---
+++ backend/src/models/School.model.ts
@@ -0,0 +1,51 @@
+import { DataTypes, Optional, Model } from "sequelize";
+import { sequelize } from "../config/database";
+
+interface SchoolAttributes {
+    id: number;
+    user_id:number;
+    name: string;
+    academic_year: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export type SchoolCreationAttributes = Optional<SchoolAttributes, 'id' | 'createdAt' | 'updatedAt'>;
+
+export class School extends Model<SchoolAttributes, SchoolCreationAttributes>
+    implements SchoolAttributes {
+    public id!: number;
+    public user_id!:number;
+    public name!: string;
+    public academic_year!: string;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+}
+
+School.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            autoIncrement:true,
+            primaryKey:true,
+        },
+        user_id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        academic_year:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+        },
+    },
+    {
+        sequelize,
+        tableName:'schools',
+        timestamps:true,
+        underscored:true,
+    }
+);
