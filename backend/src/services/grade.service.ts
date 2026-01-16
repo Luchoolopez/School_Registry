e2e7f908c4cd4ce2eb9c2819f3e294c14a02681f
LuchooLopez <luchoolopez2810@gmail.com>
2026-01-16 02:49:50 -0300

    creado grade service

---
+++ backend/src/services/grade.service.ts
@@ -0,0 +1,73 @@
+import { Grade } from "../models/Grade.model";
+import { Student } from "../models/Student.model";
+import { School } from "../models/School.model";
+import { CreateGradeInput, UpdateGradeInput } from "../validations/grade.schema";
+
+export class GradeService {
+    async addGrade(userId: number, studentId: number, data: CreateGradeInput) {
+        const student = await Student.findOne({
+            where: { id: studentId },
+            include:[{
+                model: School,
+                as: 'school',
+                where: { user_id: userId },
+                required:true
+            }]
+        });
+
+        if(!student){
+            throw new Error('Alumno no encontrado o no tienes acceso');
+        }
+
+        return await Grade.create({
+            student_id: studentId,
+            concept: data.concept,
+            value: data.value,
+            date: data.date
+        });
+    }
+
+    async updateGrade(userId: number, gradeId: number, data: UpdateGradeInput) {
+        const grade = await Grade.findOne({
+            where: { id: gradeId },
+            include:[{
+                model: Student,
+                as: 'student',
+                include:[{
+                    model: School,
+                    as: 'school',
+                    where: { user_id: userId },
+                    required: true
+                }]
+            }]
+        });
+
+        if(!grade){
+            throw new Error('Nota no encontrada o no tienes acceso');
+        }
+
+        return await grade.update(data);
+    }
+
+    async deleteGrade(userId: number, gradeId: number) {
+        const grade = await Grade.findOne({
+            where: { id: gradeId },
+            include: [{
+                model: Student,
+                as: 'student',
+                include: [{
+                    model: School,
+                    as: 'school',
+                    where: { user_id: userId }
+                }]
+            }]
+        });
+
+        if(!grade){
+            throw new Error('Nota no encontrada o no tienes permisos');
+        }
+
+        await grade.destroy();
+        return true;
+    }
+}
