4c8d9c0f807cfe7bdb3069d6496f4587d81c9cb0
LuchooLopez <luchoolopez2810@gmail.com>
2026-01-17 20:48:47 -0300

    eliminado comentarios

--- frontend/src/components/students/AttendanceTab.tsx
+++ frontend/src/components/students/AttendanceTab.tsx
@@ -46,7 +46,6 @@ export const AttendanceTab: React.FC<Props> = ({ student, onRefresh }) => {
          <h3 className="text-sm font-bold text-slate-700 dark:text-slate-300">Registro de Ausencias</h3>
          
          <div className="flex items-center gap-2">
-           {/* Botón Nueva Falta */}
            <button 
               onClick={() => setIsAdding(true)} 
               className="text-xs flex items-center gap-1 text-primary hover:bg-primary/10 px-3 py-1.5 rounded-lg transition-colors font-bold"
@@ -55,9 +54,8 @@ export const AttendanceTab: React.FC<Props> = ({ student, onRefresh }) => {
               Nueva Falta
            </button>
 
-           {/* Botón Calcular Promedio */}
            <button 
-              onClick={() => setIsCalculating(true)} // <--- Abre el modal
+              onClick={() => setIsCalculating(true)} 
               className="text-xs flex items-center gap-1 text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 px-3 py-1.5 rounded-lg transition-colors font-medium border border-slate-200 dark:border-slate-700"
            >
               <span className="material-symbols-outlined text-[18px]">calculate</span> 
@@ -111,7 +109,6 @@ export const AttendanceTab: React.FC<Props> = ({ student, onRefresh }) => {
           </table>
        </div>
 
-       {/* --- MODALES --- */}
        
        <CreateAbsenceModal 
           isOpen={isAdding}
@@ -119,11 +116,10 @@ export const AttendanceTab: React.FC<Props> = ({ student, onRefresh }) => {
           onSave={handleAdd}
        />
        
-       {/* NUEVO MODAL DE CÁLCULO */}
        <CalculateAttendanceModal 
           isOpen={isCalculating}
           onClose={() => setIsCalculating(false)}
-          currentAbsencesCount={student.absences?.length || 0} // Pasamos la cantidad actual
+          currentAbsencesCount={student.absences?.length || 0} 
        />
 
        <ConfirmModal 
